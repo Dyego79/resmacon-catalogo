5c96758831d102caad19b08ecc3ff3b2a2b4534f
Diego Perez <dyego79@gmail.com>
2025-01-03 10:41:02 -0300

    inicio2

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   //base: "/catalogo/",
-  /* build: {
+  build: {
     sourcemap: false,
-  }, */
+  },
   plugins: [react()],
 });
